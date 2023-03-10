7eb0709eb312d9e6041e220e2d42f9d9f136c96d
김은영 <66293497+aldo814@users.noreply.github.com>
2023-03-10 09:22:47 +0900

    메뉴 변경

--- html/js/run.js
+++ html/js/run.js
@@ -6,20 +6,20 @@ $(document).ready(function () {
 
   
 	//Header
-	$('.gnb, .mega_menu').mouseenter(function () {
-		$(".all_menu").addClass('active');
-		$('.mega_menu').stop().slideDown(300)
-	});
-    
-    $('.gnb, .mega_menu').mouseleave(function () {
-		$(".all_menu").removeClass('active');
-		$('.mega_menu').stop().slideUp(300)
-	});
-
     if (window.innerWidth < 1024) {
         $('#header .header-nav .all_menu a').click(function () {
             $('body').toggleClass('lock');
         });
+    } else {
+    	$('.gnb, .mega_menu').mouseenter(function () {
+    		$(".all_menu").addClass('active');
+    		$('.mega_menu').stop().slideDown(300)
+    	});
+        
+        $('.gnb, .mega_menu').mouseleave(function () {
+    		$(".all_menu").removeClass('active');
+    		$('.mega_menu').stop().slideUp(300)
+    	});
     }
 
 
